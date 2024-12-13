933feed5d1c93ab2d159c9b4731add0d93ed6ad5
Katelyn Sung <sungkate@hawaii.edu>
2024-12-12 20:36:18 -1000

    updated products page w real images and updated orderform images

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,33 +21,43 @@ const ProductCard = ({ jewel }: { jewel: Jewels }) => {
   }
 
   return (
-    <Card>
-      <Card.Header>
-        <Image className="mx-auto d-block" src={jewel.image} width={200} />
-        <Card.Title>{jewel.name}</Card.Title>
-        <Card.Subtitle>
-          $
-          {' '}
-          {jewel.price}
-        </Card.Subtitle>
-        <Card.Body>
-          <Card.Text>{jewel.description}</Card.Text>
-        </Card.Body>
-      </Card.Header>
-      <Card.Footer>
-        <Button
-          type="button"
-          onClick={() => {
-            if (currentUser) {
-              AddToCart({ userEmail: currentUser, jewel: [jewel.name] });
-            }
-          }}
-          className="btn btn-warning float-right"
-        >
-          Add to Cart
-        </Button>
-      </Card.Footer>
-    </Card>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <Card className="small-card">
+            <Card.Header>
+              <Image
+                className="mx-auto d-block"
+                src={jewel.image}
+                style={{ width: '260px', height: '260px', objectFit: 'cover' }}
+              />
+              <Card.Title className="text-center cursivetext">{jewel.name}</Card.Title>
+              <Card.Subtitle className="text-center cursivetext">
+                $
+                {' '}
+                {jewel.price}
+              </Card.Subtitle>
+              <Card.Body>
+                <Card.Text className="text-center cursivetext">{jewel.description}</Card.Text>
+              </Card.Body>
+            </Card.Header>
+            <Card.Footer>
+              <Button
+                type="button"
+                onClick={() => {
+                  if (currentUser) {
+                    AddToCart({ userEmail: currentUser, jewel: [jewel.name] });
+                  }
+                }}
+                className="btn btn-warning mx-auto d-block float-right"
+              >
+                Add to Cart
+              </Button>
+            </Card.Footer>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 };
 

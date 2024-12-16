8cc9404fc1a9a25c4ff090b9a5223506b334daef
jarrenseson <jarrenys@hawaii.edu>
2024-12-15 16:03:24 -1000

    made tests

--- tests/admin-pages.spec.ts
+++ tests/admin-pages.spec.ts
@@ -1,21 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test.use({
   storageState: 'admin-auth.json',
 });
 
-test('Admin Pages', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('link', { name: 'Next.js Application Template' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Add Stuff' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'List Stuff' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'admin@foo.com' })).toBeVisible();
-  await page.getByRole('link', { name: 'Add Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Add Stuff' })).toBeVisible();
-  await page.getByRole('link', { name: 'List Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Stuff' })).toBeVisible();
-  await page.getByRole('link', { name: 'Admin' }).click();
-  await expect(page.getByRole('heading', { name: 'List Stuff Admin' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'List Users Admin' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/auth/signin');
+  await page.getByPlaceholder('Enter your email').click();
+  await page.getByPlaceholder('Enter your email').fill('admin@foo.com');
+  await page.getByPlaceholder('Enter your email').press('Tab');
+  await page.getByPlaceholder('Enter your password').fill('changeme');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('link', { name: 'List New Products' }).click();
+  await page.locator('input[name="name"]').click();
+  await page.locator('input[name="name"]').fill('a');
+  await page.locator('input[name="name"]').click();
+  await page.locator('input[name="name"]').fill('adfa');
+  await page.locator('input[name="name"]').press('Tab');
+  await page.getByRole('spinbutton').fill('3');
+  await page.getByRole('spinbutton').press('Tab');
+  await page.locator('input[name="description"]').fill('dsaf');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.locator('input[name="image"]').click();
+  await page.locator('input[name="image"]').fill('asdf');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
 });
--- tests/cart-page.spec.ts
+++ tests/cart-page.spec.ts
@@ -6,20 +6,13 @@ test.use({
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/auth/signin');
-  await page.locator('input[name="email"]').click();
-  await page.locator('input[name="email"]').fill('john@foo.coim');
-  await page.locator('input[name="email"]').press('Tab');
-  await page.locator('input[name="email"]').click();
-  await page.locator('input[name="email"]').fill('john@foo.com');
-  await page.locator('input[name="email"]').press('Tab');
-  await page.locator('input[name="email"]').click();
-  await page.locator('input[name="password"]').click();
-  await page.locator('input[name="password"]').press('CapsLock');
-  await page.locator('input[name="password"]').fill('B');
-  await page.locator('input[name="password"]').press('CapsLock');
-  await page.locator('input[name="password"]').fill('Bob32145@');
-  await page.locator('input[name="password"]').press('Enter');
-  await page.getByRole('button', { name: 'Signin' }).click();
-  await page.locator('#basic-navbar-nav').getByRole('link').nth(3).click();
-  await page.getByRole('link', { name: 'Proceed to Payment' }).click();
+  await page.getByPlaceholder('Enter your email').click();
+  await page.getByPlaceholder('Enter your email').fill('john@foo.com');
+  await page.getByPlaceholder('Enter your email').press('Tab');
+  await page.getByPlaceholder('Enter your password').fill('changeme');
+  await page.getByPlaceholder('Enter your password').press('Enter');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('link').nth(3).click();
+  await page.getByRole('button', { name: 'Proceed to Payment' }).click();
+  await page.goto('http://localhost:3000/payment');
 });
